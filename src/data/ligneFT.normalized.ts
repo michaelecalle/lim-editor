ca63f5386d118fe2efb3d5f181ff7267432cbed5
michaelecalle <michael@radioequinoxe.com>
2026-03-22 17:09:09 +0100

    Publish updated ligneFT.normalized.ts

--- src/data/ligneFT.normalized.ts
+++ src/data/ligneFT.normalized.ts
@@ -983,7 +983,7 @@ export const LIGNE_FT_NORMALIZED: LigneFTNormalized = {
       {
         "id": "ns-data-0055",
         "type": "data",
-        "reseau": "RFN",
+        "reseau": "test",
         "pkInterne": "805.5",
         "pkAdif": "",
         "pkLfp": "",
