6d539243cdc8430b119c6078e28a7b25fae25c42
michaelecalle <michael@radioequinoxe.com>
2026-03-25 14:32:32 +0100

    chemin

--- src/lib/ligneft/constants.ts
+++ src/lib/ligneft/constants.ts
@@ -2,6 +2,10 @@ export const ACTIVE_FILE_PATH = "src/data/ligneFT.normalized.ts";
 
 export const ACTIVE_JSON_FILE_PATH = "src/data/ligneFT.normalized.json";
 
+export const LIM2_ACTIVE_FILE_PATH = "src/data/normalized/ligneFT.normalized.ts";
+
+export const LIM2_ACTIVE_JSON_FILE_PATH = "src/data/ligneFT.normalized.json";
+
 export const ARCHIVES_DIR_PATH = "src/data/archives/ligneft";
 
 export const MAX_ARCHIVES = 10;
